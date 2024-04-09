c2db9416bab487f6ed510b58160be658642da95d
Nimit Patel <nimitpatelnp19@gmail.com>
2024-04-09 18:51:37 +0530

    add: Lapinoz images

--- Restaurants.js
+++ Restaurants.js
@@ -1,30 +1,35 @@
+// --------------------------------------------------------------------------------
+
+// --------------------------------------------------------------------------------
+
 const Restaurants = [
   {
-    id: "s1",
-    shopName: "La Pino'z Pizza",
-    shopDescription: "Pizza, Fast Food, Beverages",
-    shopLogoUrl:
+    shop_name: "La Pino'z Pizza",
+    shop_description: "Pizza, Fast Food, Beverages",
+    shop_logo_url:
       "https://github.com/Nimit19/foodPlazaImages/blob/master/FoodShopsLogo/lapinozLogo.jpg?raw=true",
-    shopBg: {
+    shop_bg: {
       bg1: "https://github.com/Nimit19/foodPlazaImages/blob/master/FoodImages/Lapinoz/bg1.jpg?raw=true",
       bg2: "https://github.com/Nimit19/foodPlazaImages/blob/master/FoodImages/Lapinoz/bg2.jpg?raw=true",
       bg3: "https://github.com/Nimit19/foodPlazaImages/blob/master/FoodImages/Lapinoz/bg3.jpg?raw=true",
     },
-    menuPage: [
+    menu_page: [
       {
-        menuPageName: "Takeaway",
-        menuPageImage: "",
+        menu_page_name: "Takeaway",
+        menu_page_image:
+          "https://github.com/Nimit19/foodPlazaImages/blob/master/FoodImages/Lapinoz/menu1.jpg?raw=true",
       },
       {
-        menuPageName: "Dining",
-        menuPageImage: "",
+        menu_page_name: "Dining",
+        menu_page_image:
+          "https://github.com/Nimit19/foodPlazaImages/blob/master/FoodImages/Lapinoz/menu1.jpg?raw=true",
       },
     ],
-    isOpen: true,
-    location: "",
-    openingTime: "11:00 AM",
-    closingTime: "11:00 PM",
-    averageCost: "700rs per 2 person",
+    is_open: true,
+    location: { type: "Point", coordinates: [21.1702, 72.8311] }, // Example coordinates for Surat
+    opening_time: "11:00 AM",
+    closing_time: "11:00 PM",
+    average_cost: "700rs per 2 person",
     ratings: 4.5,
     address: {
       line1: "A-301, Swaminarayan Society",
@@ -37,31 +42,32 @@ const Restaurants = [
     },
   },
   {
-    id: "s2",
-    shopName: "McDonald's",
-    shopDescription: "Burgers, Fast Food, Beverages",
-    shopLogoUrl:
+    shop_name: "McDonald's",
+    shop_description: "Burgers, Fast Food, Beverages",
+    shop_logo_url:
       "https://github.com/Nimit19/foodPlazaImages/blob/master/FoodShopsLogo/macD.png?raw=true",
-    shopBg: {
-      bg1: "",
-      bg2: "",
-      bg3: "",
+    shop_bg: {
+      bg1: "https://github.com/Nimit19/foodPlazaImages/blob/master/FoodImages/Lapinoz/bg1.jpg?raw=true",
+      bg2: "https://github.com/Nimit19/foodPlazaImages/blob/master/FoodImages/Lapinoz/bg2.jpg?raw=true",
+      bg3: "https://github.com/Nimit19/foodPlazaImages/blob/master/FoodImages/Lapinoz/bg3.jpg?raw=true",
     },
-    menuPage: [
+    menu_page: [
       {
-        menuPageName: "",
-        menuPageImage: "",
+        menu_page_name: "Takeaway",
+        menu_page_image:
+          "https://github.com/Nimit19/foodPlazaImages/blob/master/FoodImages/Lapinoz/menu1.jpg?raw=true",
       },
       {
-        menuPageName: "",
-        menuPageImage: "",
+        menu_page_name: "Dining",
+        menu_page_image:
+          "https://github.com/Nimit19/foodPlazaImages/blob/master/FoodImages/Lapinoz/menu1.jpg?raw=true",
       },
     ],
-    isOpen: true,
-    location: "",
-    openingTime: "10:00 AM",
-    closingTime: "11:00 PM",
-    averageCost: "200rs per person",
+    is_open: true,
+    location: { type: "Point", coordinates: [21.1702, 72.8311] }, // Example coordinates for Surat
+    opening_time: "10:00 AM",
+    closing_time: "11:00 PM",
+    average_cost: "200rs per person",
     address: {
       line1: "B-102, Rajhans Residency",
       line2: "Opposite VR Mall",
@@ -69,35 +75,36 @@ const Restaurants = [
       state: "Gujarat",
       pincode: "395009",
       country: "India",
-      isPrimary: false,
+      isPrimary: true,
     },
   },
   {
-    id: "s3",
-    shopName: "STARBUCKS",
-    shopDescription: "Coffee, Tea, Bakery",
-    shopLogoUrl:
+    shop_name: "STARBUCKS",
+    shop_description: "Coffee, Tea, Bakery",
+    shop_logo_url:
       "https://github.com/Nimit19/foodPlazaImages/blob/master/FoodShopsLogo/starbucks.png?raw=true",
-    shopBg: {
-      bg1: "",
-      bg2: "",
-      bg3: "",
+    shop_bg: {
+      bg1: "https://github.com/Nimit19/foodPlazaImages/blob/master/FoodImages/Lapinoz/bg1.jpg?raw=true",
+      bg2: "https://github.com/Nimit19/foodPlazaImages/blob/master/FoodImages/Lapinoz/bg2.jpg?raw=true",
+      bg3: "https://github.com/Nimit19/foodPlazaImages/blob/master/FoodImages/Lapinoz/bg3.jpg?raw=true",
     },
-    menuPage: [
+    menu_page: [
       {
-        menuPageName: "",
-        menuPageImage: "",
+        menu_page_name: "Takeaway",
+        menu_page_image:
+          "https://github.com/Nimit19/foodPlazaImages/blob/master/FoodImages/Lapinoz/menu1.jpg?raw=true",
       },
       {
-        menuPageName: "",
-        menuPageImage: "",
+        menu_page_name: "Dining",
+        menu_page_image:
+          "https://github.com/Nimit19/foodPlazaImages/blob/master/FoodImages/Lapinoz/menu1.jpg?raw=true",
       },
     ],
-    isOpen: true,
-    location: "",
-    openingTime: "8:00 AM",
-    closingTime: "10:00 PM",
-    averageCost: "300rs per person",
+    is_open: true,
+    location: { type: "Point", coordinates: [21.1702, 72.8311] }, // Example coordinates for Surat
+    opening_time: "8:00 AM",
+    closing_time: "10:00 PM",
+    average_cost: "300rs per person",
     address: {
       line1: "C-203, Diamond Enclave",
       line2: "Near Athwa Gate",
@@ -105,35 +112,36 @@ const Restaurants = [
       state: "Gujarat",
       pincode: "395001",
       country: "India",
-      isPrimary: false,
+      isPrimary: true,
     },
   },
   {
-    id: "s4",
-    shopName: "Burger King",
-    shopDescription: "Burgers, Fast Food, Beverages",
-    shopLogoUrl:
+    shop_name: "Burger King",
+    shop_description: "Burgers, Fast Food, Beverages",
+    shop_logo_url:
       "https://github.com/Nimit19/foodPlazaImages/blob/master/FoodShopsLogo/burgerKing.png?raw=true",
-    shopBg: {
-      bg1: "",
-      bg2: "",
-      bg3: "",
+    shop_bg: {
+      bg1: "https://github.com/Nimit19/foodPlazaImages/blob/master/FoodImages/Lapinoz/bg1.jpg?raw=true",
+      bg2: "https://github.com/Nimit19/foodPlazaImages/blob/master/FoodImages/Lapinoz/bg2.jpg?raw=true",
+      bg3: "https://github.com/Nimit19/foodPlazaImages/blob/master/FoodImages/Lapinoz/bg3.jpg?raw=true",
     },
-    menuPage: [
+    menu_page: [
       {
-        menuPageName: "",
-        menuPageImage: "",
+        menu_page_name: "Takeaway",
+        menu_page_image:
+          "https://github.com/Nimit19/foodPlazaImages/blob/master/FoodImages/Lapinoz/menu1.jpg?raw=true",
       },
       {
-        menuPageName: "",
-        menuPageImage: "",
+        menu_page_name: "Dining",
+        menu_page_image:
+          "https://github.com/Nimit19/foodPlazaImages/blob/master/FoodImages/Lapinoz/menu1.jpg?raw=true",
       },
     ],
-    isOpen: true,
-    location: "",
-    openingTime: "10:30 AM",
-    closingTime: "10:00 PM",
-    averageCost: "250rs per person",
+    is_open: true,
+    location: { type: "Point", coordinates: [21.1702, 72.8311] }, // Example coordinates for Surat
+    opening_time: "10:30 AM",
+    closing_time: "10:00 PM",
+    average_cost: "250rs per person",
     address: {
       line1: "D-401, Shanti Apartment",
       line2: "Behind Iscon Mall",
@@ -141,35 +149,36 @@ const Restaurants = [
       state: "Gujarat",
       pincode: "395002",
       country: "India",
-      isPrimary: false,
+      isPrimary: true,
     },
   },
   {
-    id: "s5",
-    shopName: "KFC",
-    shopDescription: "Chicken, Burgers, Fast Food",
-    shopLogoUrl:
+    shop_name: "KFC",
+    shop_description: "Chicken, Burgers, Fast Food",
+    shop_logo_url:
       "https://github.com/Nimit19/foodPlazaImages/blob/master/FoodShopsLogo/kfc.png?raw=true",
-    shopBg: {
-      bg1: "",
-      bg2: "",
-      bg3: "",
+    shop_bg: {
+      bg1: "https://github.com/Nimit19/foodPlazaImages/blob/master/FoodImages/Lapinoz/bg1.jpg?raw=true",
+      bg2: "https://github.com/Nimit19/foodPlazaImages/blob/master/FoodImages/Lapinoz/bg2.jpg?raw=true",
+      bg3: "https://github.com/Nimit19/foodPlazaImages/blob/master/FoodImages/Lapinoz/bg3.jpg?raw=true",
     },
-    menuPage: [
+    menu_page: [
       {
-        menuPageName: "",
-        menuPageImage: "",
+        menu_page_name: "Takeaway",
+        menu_page_image:
+          "https://github.com/Nimit19/foodPlazaImages/blob/master/FoodImages/Lapinoz/menu1.jpg?raw=true",
       },
       {
-        menuPageName: "",
-        menuPageImage: "",
+        menu_page_name: "Dining",
+        menu_page_image:
+          "https://github.com/Nimit19/foodPlazaImages/blob/master/FoodImages/Lapinoz/menu1.jpg?raw=true",
       },
     ],
-    isOpen: true,
-    location: "",
-    openingTime: "10:00 AM",
-    closingTime: "11:00 PM",
-    averageCost: "300rs per person",
+    is_open: true,
+    location: { type: "Point", coordinates: [21.1702, 72.8311] }, // Example coordinates for Surat
+    opening_time: "10:00 AM",
+    closing_time: "11:00 PM",
+    average_cost: "300rs per person",
     address: {
       line1: "E-301, Nandanvan Society",
       line2: "Near Adajan",
@@ -177,35 +186,36 @@ const Restaurants = [
       state: "Gujarat",
       pincode: "395003",
       country: "India",
-      isPrimary: false,
+      isPrimary: true,
     },
   },
   {
-    id: "s6",
-    shopName: "Coffee Culture",
-    shopDescription: "Coffee, Tea, Snacks",
-    shopLogoUrl:
+    shop_name: "Coffee Culture",
+    shop_description: "Coffee, Tea, Snacks",
+    shop_logo_url:
       "https://github.com/Nimit19/foodPlazaImages/blob/master/FoodShopsLogo/coffieCulture.png?raw=true",
-    shopBg: {
-      bg1: "",
-      bg2: "",
-      bg3: "",
+    shop_bg: {
+      bg1: "https://github.com/Nimit19/foodPlazaImages/blob/master/FoodImages/Lapinoz/bg1.jpg?raw=true",
+      bg2: "https://github.com/Nimit19/foodPlazaImages/blob/master/FoodImages/Lapinoz/bg2.jpg?raw=true",
+      bg3: "https://github.com/Nimit19/foodPlazaImages/blob/master/FoodImages/Lapinoz/bg3.jpg?raw=true",
     },
-    menuPage: [
+    menu_page: [
       {
-        menuPageName: "",
-        menuPageImage: "",
+        menu_page_name: "Takeaway",
+        menu_page_image:
+          "https://github.com/Nimit19/foodPlazaImages/blob/master/FoodImages/Lapinoz/menu1.jpg?raw=true",
       },
       {
-        menuPageName: "",
-        menuPageImage: "",
+        menu_page_name: "Dining",
+        menu_page_image:
+          "https://github.com/Nimit19/foodPlazaImages/blob/master/FoodImages/Lapinoz/menu1.jpg?raw=true",
       },
     ],
-    isOpen: true,
-    location: "",
-    openingTime: "7:00 AM",
-    closingTime: "11:00 PM",
-    averageCost: "150rs per person",
+    is_open: true,
+    location: { type: "Point", coordinates: [21.1702, 72.8311] }, // Example coordinates for Surat
+    opening_time: "7:00 AM",
+    closing_time: "11:00 PM",
+    average_cost: "150rs per person",
     address: {
       line1: "F-102, Jalaram Complex",
       line2: "Opposite Railway Station",
@@ -213,35 +223,36 @@ const Restaurants = [
       state: "Gujarat",
       pincode: "395004",
       country: "India",
-      isPrimary: false,
+      isPrimary: true,
     },
   },
   {
-    id: "s7",
-    shopName: "Malhar Dosa",
-    shopDescription: "South Indian Food, Dosas', Idlis'",
-    shopLogoUrl:
+    shop_name: "Malhar Dosa",
+    shop_description: "South Indian Food, Dosas', Idlis'",
+    shop_logo_url:
       "https://github.com/Nimit19/foodPlazaImages/blob/master/FoodShopsLogo/malhar.png?raw=true",
-    shopBg: {
-      bg1: "",
-      bg2: "",
-      bg3: "",
+    shop_bg: {
+      bg1: "https://github.com/Nimit19/foodPlazaImages/blob/master/FoodImages/Lapinoz/bg1.jpg?raw=true",
+      bg2: "https://github.com/Nimit19/foodPlazaImages/blob/master/FoodImages/Lapinoz/bg2.jpg?raw=true",
+      bg3: "https://github.com/Nimit19/foodPlazaImages/blob/master/FoodImages/Lapinoz/bg3.jpg?raw=true",
     },
-    menuPage: [
+    menu_page: [
       {
-        menuPageName: "",
-        menuPageImage: "",
+        menu_page_name: "Takeaway",
+        menu_page_image:
+          "https://github.com/Nimit19/foodPlazaImages/blob/master/FoodImages/Lapinoz/menu1.jpg?raw=true",
       },
       {
-        menuPageName: "",
-        menuPageImage: "",
+        menu_page_name: "Dining",
+        menu_page_image:
+          "https://github.com/Nimit19/foodPlazaImages/blob/master/FoodImages/Lapinoz/menu1.jpg?raw=true",
       },
     ],
-    isOpen: true,
-    location: "",
-    openingTime: "8:00 AM",
-    closingTime: "10:00 PM",
-    averageCost: "100rs per person",
+    is_open: true,
+    location: { type: "Point", coordinates: [21.1702, 72.8311] }, // Example coordinates for Surat
+    opening_time: "8:00 AM",
+    closing_time: "10:00 PM",
+    average_cost: "100rs per person",
     address: {
       line1: "G-201, Aakash Residency",
       line2: "Near Vesu",
@@ -249,35 +260,36 @@ const Restaurants = [
       state: "Gujarat",
       pincode: "395006",
       country: "India",
-      isPrimary: false,
+      isPrimary: true,
     },
   },
   {
-    id: "s8",
-    shopName: "Tea Post",
-    shopDescription: "Tea, Snacks, Beverages",
-    shopLogoUrl:
+    shop_name: "Tea Post",
+    shop_description: "Tea, Snacks, Beverages",
+    shop_logo_url:
       "https://github.com/Nimit19/foodPlazaImages/blob/master/FoodShopsLogo/teaPost.png?raw=true",
-    shopBg: {
-      bg1: "",
-      bg2: "",
-      bg3: "",
+    shop_bg: {
+      bg1: "https://github.com/Nimit19/foodPlazaImages/blob/master/FoodImages/Lapinoz/bg1.jpg?raw=true",
+      bg2: "https://github.com/Nimit19/foodPlazaImages/blob/master/FoodImages/Lapinoz/bg2.jpg?raw=true",
+      bg3: "https://github.com/Nimit19/foodPlazaImages/blob/master/FoodImages/Lapinoz/bg3.jpg?raw=true",
     },
-    menuPage: [
+    menu_page: [
       {
-        menuPageName: "",
-        menuPageImage: "",
+        menu_page_name: "Takeaway",
+        menu_page_image:
+          "https://github.com/Nimit19/foodPlazaImages/blob/master/FoodImages/Lapinoz/menu1.jpg?raw=true",
       },
       {
-        menuPageName: "",
-        menuPageImage: "",
+        menu_page_name: "Dining",
+        menu_page_image:
+          "https://github.com/Nimit19/foodPlazaImages/blob/master/FoodImages/Lapinoz/menu1.jpg?raw=true",
       },
     ],
-    isOpen: true,
-    location: "",
-    openingTime: "7:30 AM",
-    closingTime: "11:00 PM",
-    averageCost: "80rs per person",
+    is_open: true,
+    location: { type: "Point", coordinates: [21.1702, 72.8311] }, // Example coordinates for Surat
+    opening_time: "7:30 AM",
+    closing_time: "11:00 PM",
+    average_cost: "80rs per person",
     address: {
       line1: "H-401, Panchvati Society",
       line2: "Near Piplod",
@@ -285,35 +297,36 @@ const Restaurants = [
       state: "Gujarat",
       pincode: "395010",
       country: "India",
-      isPrimary: false,
+      isPrimary: true,
     },
   },
   {
-    id: "s9",
-    shopName: "Subway",
-    shopDescription: "Sandwiches, Salads, Wraps",
-    shopLogoUrl:
+    shop_name: "Subway",
+    shop_description: "Sandwiches, Salads, Wraps",
+    shop_logo_url:
       "https://github.com/Nimit19/foodPlazaImages/blob/master/FoodShopsLogo/subway.png?raw=true",
-    shopBg: {
-      bg1: "",
-      bg2: "",
-      bg3: "",
+    shop_bg: {
+      bg1: "https://github.com/Nimit19/foodPlazaImages/blob/master/FoodImages/Lapinoz/bg1.jpg?raw=true",
+      bg2: "https://github.com/Nimit19/foodPlazaImages/blob/master/FoodImages/Lapinoz/bg2.jpg?raw=true",
+      bg3: "https://github.com/Nimit19/foodPlazaImages/blob/master/FoodImages/Lapinoz/bg3.jpg?raw=true",
     },
-    menuPage: [
+    menu_page: [
       {
-        menuPageName: "",
-        menuPageImage: "",
+        menu_page_name: "Takeaway",
+        menu_page_image:
+          "https://github.com/Nimit19/foodPlazaImages/blob/master/FoodImages/Lapinoz/menu1.jpg?raw=true",
       },
       {
-        menuPageName: "",
-        menuPageImage: "",
+        menu_page_name: "Dining",
+        menu_page_image:
+          "https://github.com/Nimit19/foodPlazaImages/blob/master/FoodImages/Lapinoz/menu1.jpg?raw=true",
       },
     ],
-    isOpen: true,
-    location: "",
-    openingTime: "10:00 AM",
-    closingTime: "10:00 PM",
-    averageCost: "250rs per person",
+    is_open: true,
+    location: { type: "Point", coordinates: [21.1702, 72.8311] }, // Example coordinates for Surat
+    opening_time: "10:00 AM",
+    closing_time: "10:00 PM",
+    average_cost: "250rs per person",
     address: {
       line1: "I-203, Harmony Heights",
       line2: "Near Pal",
@@ -321,7 +334,7 @@ const Restaurants = [
       state: "Gujarat",
       pincode: "395005",
       country: "India",
-      isPrimary: false,
+      isPrimary: true,
     },
   },
 ];
